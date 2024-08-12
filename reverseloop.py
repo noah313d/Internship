c1a7144f0961e55fdf9b91ccd0eb9e8a4303bc84
noah313d <theamazingnd12@outlook.com>
2024-08-12 12:54:14 -0400

    commit

--- reverseloop.py
+++ reverseloop.py
@@ -1,4 +1,5 @@
 #reversing without chaning the list itself.
-sentence = rev.list(["the", "quick", "brown", "fox", "jumped", "over",])
-for word in sentence:
+from audioop import reverse
+sentence = reverse.list(["the", "quick", "brown", "fox", "jumped", "over",])
+def new_func(word):
     print(word)
