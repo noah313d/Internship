5ad74e886d0eb0d7e5c96c69582e1ebc59588e3c
noah313d <theamazingnd12@outlook.com>
2024-08-20 13:59:45 -0400

    commit

---
+++ code/homerun.py
@@ -0,0 +1,13 @@
+#players_stats = [] 
+#players_stats.append({"name": "Aaron Judge", "Games": 123, "Home Runs": 44})
+#players_stats.append({"name": "Juan Soto", "Games": 121, "Home Runs": 34})
+#players_stats.append({"name": "Bobby Witt", "Games": 124, "Home Runs": 25})
+#players_stats.append({"name": "Shohei Ohtani", "Games":122, "Home Runs": 39})
+#players_stats.append({"name": "Marcell Ozuna", "Games": 124, "Home Runs": 36})
+#players_stats.append({"name": "Rafael Devers", "Games": 110, "Home Runs": 27})
+#players_stats.append({"name": "Yordan Alvarez", "Games": 117, "Home Runs": 25})
+#players_stats.append({"name": "Brent Rooker", "Games": 107, "Home Runs": 29})
+#players_stats.append({"name": "Gunnar Henderson", "Games": 123, "Home Runs": 33})
+#players_stats.append({"name": "Vladimir Guerrero", "Games": 123, "Home Runs": 25})
+
+print.percentage({"44/123", "34/121", "25/124", "39/122", "36/124", "27/110", "25/117", "29/107", "33/123", "25/123"})
